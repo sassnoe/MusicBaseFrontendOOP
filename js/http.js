a83e33d023c11b14427023b3c1e89cf69975e50c
buan0001 <bustermikkel@gmail.com>
2023-10-12 00:19:30 +0200

    copied stuff

--- js/http.js
+++ js/http.js
@@ -4,13 +4,41 @@ import Artists from "./model/artists.js";
 const port = 3333;
 const endpoint = `http://localhost:${port}`;
 
-async function getArtists() {
-  const response = await fetch(`${endpoint}/artists`);
-  const objects = await response.json();
-  //   console.log(req);
-  const artistsList = objects.map((jsonObj) => new Artists(jsonObj));
-  console.log(artistsList);
-  return artistsList;
+async function searchDatabase(whereToSearch, searchValue) {
+  const response = await fetch(`${endpoint}/${whereToSearch}/search?q=${searchValue}`);
+  const tracksData = await response.json();
+
+  if (tracksData.tracks) {
+    for (const track of tracksData.tracks) {
+      track.durationSeconds = secondsToMinutesAndSeconds(track.durationSeconds);
+    }
+  }
+  return tracksData;
 }
 
-export { getArtists };
+async function findAlbumsByArtist(whereToSearch, searchID) {
+  const response = await fetch(`${endpoint}/${whereToSearch}/search/${searchID}`);
+  return await response.json();
+}
+
+async function findTracksByAlbum(whereToSearch, searchID) {
+  const response = await fetch(`${endpoint}/${whereToSearch}/${searchID}`);
+  const tracksData = await response.json();
+  for (const track of tracksData.tracks) {
+    track.durationSeconds = secondsToMinutesAndSeconds(track.durationSeconds);
+  }
+  return tracksData;
+}
+
+export { searchDatabase, findAlbumsByArtist, findTracksByAlbum };
+
+// async function getArtists() {
+//   const response = await fetch(`${endpoint}/artists`);
+//   const objects = await response.json();
+//   //   console.log(req);
+//   const artistsList = objects.map((jsonObj) => new Artists(jsonObj));
+//   console.log(artistsList);
+//   return artistsList;
+// }
+
+// export { getArtists };
