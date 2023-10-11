fc9fc1aff610026062f2fbe24fa3bc3ebd0f7f8e
buan0001 <bustermikkel@gmail.com>
2023-10-11 11:26:06 +0200

    get duration()

--- js/model/tracks.js
+++ js/model/tracks.js
@@ -4,4 +4,18 @@ export default class Tracks {
     this.durationSeconds = obj.durationSeconds;
     this.artist = obj.artist
   }
+
+  get duration(){
+    // Calculate the minutes and seconds
+    let minutes = Math.floor(this.durationSeconds / 60);
+    let remainingSeconds = this.durationSeconds % 60;
+
+    // Add leading zeros if necessary
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes
+    const formattedSeconds = remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds
+
+    const formattedTime = formattedMinutes + ":" + formattedSeconds;
+
+    return formattedTime;
+  }
 }
