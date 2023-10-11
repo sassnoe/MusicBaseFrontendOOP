f316f36d3666684b23cb7f42d349216314ed4554
sassnoe <joakimsassnoe@gmail.com>
2023-10-11 12:02:09 +0200

    filter & ufærdig sort

--- js/model/albums.js
+++ js/model/albums.js
@@ -2,6 +2,6 @@ export default class Albums {
   constructor(obj) {
     this.title = obj.title;
     this.releaseYear = obj.releaseYear;
-    this.artist = obj.artist
+    this.artist = obj.artist;
   }
 }
--- js/view/listrenderer.js
+++ js/view/listrenderer.js
@@ -5,12 +5,52 @@ export default class ListRenderer {
     this.container = document.querySelector(container);
   }
 
-  render() {
-    console.log(this.list);
+  setList(list) {
+    this.list = list.map((item) => new this.itemRenderer(item));
+
+    const sortBy = this.sortBy;
+    this.sortBy = undefined;
+
+    this.sort(sortBy, this.sortDir);
+  }
+
+  clear() {
     this.container.innerHTML = "";
+  }
+
+  render() {
+    this.clear();
+
+    // create a filtered list to render
+    const filteredList = this.list.filter((item) => this.filter(item));
+
+    console.log(this.list);
     for (const item of this.list) {
       const html = this.itemRenderer.render(item);
       this.container.insertAdjacentHTML("beforeend", html);
     }
+  }
+
+  sort(sortBy, sortDir) {
+    // if sorting by the same as previous property
+    if (sortBy === this.sortDir) {
+      // toggle sort direction
+      this.sortDir = this.sortDir === "asc" ? "desc" : "asc";
+    } else {
+      if (sortDir) {
+        this.sortDir = sortDir;
+      } else {
+        this.sortDir = "asc";
+      }
+    }
+
+    this.sortBy = sortBy;
+  }
+
+  filter(filterProperty, filterValue) {
+    this.filterProperty = filterProperty;
+    this.filterValue = filterValue;
+
+    this.render();
   }
 }
