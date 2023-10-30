aab9a8f7c3b3c84ebbb6e60e6fae3e8b3e48691d
buan0001 <bustermikkel@gmail.com>
2023-10-31 00:31:34 +0100

    deleted some useless stuff

--- js/view/dialogs/general-dialogs/dialog-super.js
+++ js/view/dialogs/general-dialogs/dialog-super.js
@@ -12,33 +12,27 @@ export default class Dialog {
 
   close() {
     this.dialog.close();
-    //
   }
 
   render(detailRenderer, elementToShow, listToInclude) {
-    // console.log("LIST TO INCLUDE",listToInclude);
     this.name = detailRenderer;
-    // console.log("NAME@@@@@@@@@@@@",this.name);
     this.elementToShow = elementToShow;
     this.detailRenderer = new detailRenderer();
     const html = this.detailRenderer.render(elementToShow);
-    // console.log("html to show",html);
     this.dialog.innerHTML = html;
     if (listToInclude) {
       this.detailRenderer.fillList(listToInclude, this.dialog, elementToShow);
     }
     this.dialog.querySelector(".button-close")?.addEventListener("click", this.close.bind(this));
     this.dialog.querySelector("form")?.addEventListener("submit", this.submit.bind(this));
-    this.dialog.querySelector(".button-delete")?.addEventListener("click", () => {this.close(); deleteClicked(this.name, this.elementToShow)});
+    this.dialog.querySelector(".button-delete")?.addEventListener("click", () => {
+      this.close();
+      deleteClicked(this.name, this.elementToShow);
+    });
     this.show();
   }
 
-  // deleteClicked(){
-  //   console.log("delete clicked!");
-  // }
-
   submit(event) {
-    // console.log("THIS DIALOG", this.dialog);
     event.preventDefault();
     this.form = event.target;
     [this.submitObj, this.where] = this.detailRenderer.submit(this.form);
